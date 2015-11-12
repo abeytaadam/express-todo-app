a6fde3c0f0af8d1a49c1a233083c26b4c36f9d24
abeytaadam <measalion@gmail.com>
2015-11-11 18:39:10 -0800

    Worked on client side

---
+++ frontend/main.js
@@ -0,0 +1,55 @@
+$(function() {
+	//Compile handlebars
+	var source = $('#template').html();
+	var template = Handlebars.compile(source);
+	// set global id variable
+	var id;
+
+	// client side page info array
+	var todoList = [];
+
+	$.get('localhost:3000/api/todos', function(){
+		todoItems = data.todos;
+	});
+
+	function 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
