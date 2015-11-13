8aa6e36c9987bddc372899c22117918a7015681b
abeytaadam <measalion@gmail.com>
2015-11-12 20:51:00 -0800

    Add Post route

--- public/scripts.js
+++ public/scripts.js
@@ -1,54 +1,90 @@
 $(function() {
-	//Compile handlebars
-	var source = $('#template').html();
-	var template = Handlebars.compile(source);
-	// set global id variable
-	var id;
-
-	// client side page info array
-	var todoList = [];
-
-	$.get('localhost:3000/api/todos', function(){
-		todoItems = data.todos;
-	});
+			var id;
+			//Compile handlebars
+			var source = $('#todos-template').html();
+			var template = Handlebars.compile(source);
+			// client side page info array
+			var todosCollection = [];
+			console.log("todosCollection",todosCollection);
 
 
+			function addTodo(todoList) {
+				var html = template({
+					todos: todoList
+				});
+				$('#listCanvas').append(html);
+				document.getElementById('todoForm').reset();
+			}
+		
+			$.get('/api/todos', function(data) {
+				console.log(data);
+				var todoList = data.todos;
+				todosCollection = todoList;
+				console.log("PUSHEDTDs",todosCollection);
+				addTodo(todoList);
+			});
+			
+			$(document).on('click', '.glyphicon-pencil', function(event) {
+				event.preventDefault();
+				id = $(this).attr('id');
+				$('#form' + id).toggle();
+				console.log(id);
 
+				console.log(this);
+			});
+			
+			$('#listCanvas').on('sumbit', '.editDrop', function (event) {
+				event.preventDefault();
+				console.log(id);
+				var todoToUpdate = todosCollection.filter(function(todo) {
+					return todos._id == id;
+				})[0];
+				var editTodo = $(this).serialize();
+				console.log(editTodo);
+			
+			$.ajax({
+				type: 'PUT',
+				url: '/api/todos/' + id,
+				data: editTodo,
+				success: function(data) {
+					console.log("Edited");
+					todosCollection.splice(booksCollection.indexOf(todoToUpdate), 1, data);
+				}
+			});
+			});
 
+			$(document).on('click', '.glyphicon-remove', function (event){
+				event.preventDefault();
+				console.log('clicked');
+				var toDelete = todosCollection.filter(function(todo) {
+					return todos._id == id;
+			})[0];
+				var DeleteTodo = $(this);
+				console.log(id);
 
+			$.ajax({
+				type: 'DELETE',
+				url: '/api/todos/' + id,
+				success: function(data){
+					console.log('Deleted');
+				}
+			});
+			});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+			$('#todoForm').on('submit', function (event) {
+				event.preventDefault();
+				var newTodo = $(this).serialize();
+				
+				$.post("/api/todos", newTodo, function (data) {
+					console.log("Data", data);
+					todosCollection.push(data);
+					var todos = todosCollection;
+					$('#listCanvas').empty();
+					console.log("TODOCOLLECTION", todosCollection);
+					addTodo(todos);
+				});
+			
+			});
+			
+			
 });
--- server.js
+++ server.js
@@ -1,66 +1,69 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var hbs = require('hbs');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/todoItems');
+var Todo = require('./models/todos');
+
 
 var app = express();
 
 // Set up Body Parser
 app.use(bodyParser.urlencoded({ extended : true }));
 
-var todoList  = [
-								{
-								task : "Buy bread",
-								description : "Obtain consumable wheat product, sliced",
-								_id : 1},{
-								task : "Buy bananas",
-								description : "Obtain consumable fruit product, whole",
-								_id : 2},{
-								task : "Buy peanut butter",
-								description : "Obtain consumable legume product, whipped",
-								_id : 3
-								}];
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+// HOME PAGE ROUTE
+app.get('/', function (req, res){
+	res.render('index');
+});
 
 // Set up routes
 app.get('/api/todos', function (req, res){
-	res.json(todoList);
+
+	Todo.find(function (err, allTodos) {
+		res.json({ todos: allTodos });
+	});
 });
 
+// Finds one todo
 app.get('/api/todos/:id', function (req, res){
-	var todoId = parseInt(req.params.id);
-	var foundTodo = todoList.filter(function (todo){
-		return todos._id == todoId;
+	var todoId = req.params.id;
+	Todo.findOne({_id: todoId}, function (err, foundTodo){
+				res.json(foundTodo);
 	});
-	res.json(foundTodo);
+
 });
 
 app.post('/api/todos',function (req, res) {
-	var newTodo = req.body;
-	if (todoList.length > 0) {
-		newTodo._id = todoList[todoList.length - 1]._id + 1;
-	} else {
-		newTodo._id = 1;
-	}
-	todoList.push(newTodo);
-	res.json(newTodo);
+	var newTodo = new Todo(req.body);
+
+	newTodo.save(function(err, savedTodo){
+			res.json(savedTodo);	
+	});
+	console.log("new TODO",newTodo);
 });
 
 app.put('/api/todos/:id', function (req, res) {
-	var todoId = parseInt(req.params.id);
-	var todoToUpdate = todoList.filter(function (todo){
-		return todo._id === todoId;
-	})[0];
-	todoToUpdate.task = req.body.task;
-	todoToUpdate.description = req.body.description;
-	res.json(todoToUpdate);
+	var todoId = req.params.id;
+	Todo.findOne({ _id: todoId }, function (err, foundTodo) {
+		foundTodo.task = req.body.task;
+		foundTodo.description = req.body.description;
+		foundTodo.save(function (err, savedTodo) {
+			res.json(savedTodo);
+		});
+	});
+	
 });
 
 app.delete('/api/todos/:id', function (req, res){
-	var todoId = parseInt(req.params.id);
-	var todoToUpdate = todoList.filter(function (todo){
-		return todo._id === todoId;
-	})[0];
-	var todoIndex = myTodos.indexOf(todoToDelete);
-	myTodos.splice(todoIndex, 1);
-	res.json(todoToDelete);
+	var todoId = req.params.id;
+	console.log('todoId', todoId);
+	Todo.findOneAndRemove({_id : todoId}, function (err, deletedTodo){
+		res.json(deletedTodo);
+	});
 });
 
 // Server listening check 
